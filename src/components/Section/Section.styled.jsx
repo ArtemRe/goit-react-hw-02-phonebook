afff57d3fabed8002f067cd7793a7a9b880c0469
ArtemRe <RechembeiArtem@gmail.com>
2022-10-02 15:22:03 +0300

    cline

--- src/components/Section/Section.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const SectionWrapp = styled.section`
-  align-items: flex-start;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto 30px;
-  width: 400px;
-`;
